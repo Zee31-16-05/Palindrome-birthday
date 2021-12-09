6a0ecb6bf4b436eed300d0dd7e6df35666e00ce7
zeeshan <zeeshan311605@gmail.com>
2021-12-09 14:07:23 +0530

    script file backend for palindrome

---
+++ script.js
@@ -0,0 +1,36 @@
+function reverseString(str) {
+    var listOfChars = str.split('');
+    var listOFReverseChar = listOfChars.reverse();
+    var reversedString = listOFReverseChar.join('');
+    return reversedString;
+}
+
+function isPalindrome(str) {
+    var reversedString = reverseString(str);
+    return str === reversedString;
+}
+
+
+function getDateAsString(date) {
+
+    var dateStr = {
+        day: '',
+        month: '',
+        year: ''
+    };
+
+    if (date.day < 10) {
+        dateStr.day = '0' + date.day;
+    } else {
+        dateStr.day = date.day.toString()
+    }
+    if (date.month < 10) {
+        dateStr.month = '0' + date.month;
+    } else {
+        dateStr.month = date.month.toString()
+    }
+
+    dateStr.year = date.year.toString();
+
+    return dateStr
+}
